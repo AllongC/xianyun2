f4f7bfedd83dc7f1887b9ad299b68a7f403832b8
杨腾祺 <3150475761@qq.com>
2020-07-16 17:49:06 +0800

    17-优化输入

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -108,7 +108,7 @@ export default {
     // value 是选中的值，cb是回调函数，接收要展示的列表
     queryDepartSearch(value, suggest) {
       this.searchCity(value).then(suggestRes => {
-        if (suggestRes.length == 1) {
+        if (suggestRes.length > 0) {
           this.form.departCode = suggestRes[0].code;
         }
         suggest(suggestRes);
@@ -119,7 +119,7 @@ export default {
     // value 是选中的值，cb是回调函数，接收要展示的列表
     queryDestSearch(value, suggest) {
       this.searchCity(value).then(suggestRes => {
-        if (suggestRes.length == 1) {
+        if (suggestRes.length > 0) {
           this.form.destCode = suggestRes[0].code;
         }
         suggest(suggestRes);
