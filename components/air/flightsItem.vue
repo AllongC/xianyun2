eb40ebef631d1df7f6070de2b503f7585ab9a734
杨腾祺 <3150475761@qq.com>
2020-07-18 15:49:42 +0800

    37-订单页面和订单组件创建

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -48,7 +48,7 @@
               </el-col>
               <el-col :span="5" class="price">￥{{item.par_price}}</el-col>
               <el-col :span="3" class="choose-button">
-                <el-button type="warning" size="mini">选定</el-button>
+                <el-button type="warning" size="mini" @click="sendSet(item.seat_xid)">选定</el-button>
                 <p v-if="item.nums!=='A'">剩余：{{item.nums}}</p>
               </el-col>
             </el-row>
@@ -72,6 +72,11 @@ export default {
       default: () => {
         return {};
       }
+    }
+  },
+  methods: {
+    sendSet(index) {
+      this.$router.push(`/air/order?id=${this.data.id}&seat_xid=${index}`);
     }
   },
   computed: {
